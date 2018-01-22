2416432ce1862c08e1b0fdd4f40dbc15313d2dc1
liuliaixue <liuliaixue@qq.com>
2018-01-23 00:29:49 +0800

    u

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import BasicExmaple from './exmaple/BasicExample';
+import AuthExample from './exmaple/AuthExample';
+import customLinkExample from './exmaple/customLinkExample';
+
 class App extends Component {
   render() {
     return (
@@ -18,4 +22,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default customLinkExample;
